23d27e65a8dd6144cf6fb2028d05d39299ed1447
Otakar Jašek <otakar.jasek@avast.com>
2020-12-02 21:18:58 +0100

    first two days

---
+++ 02/main.hs
@@ -0,0 +1,32 @@
+isValidPart1 :: (Ord a1, Num a1, Eq a2) => a1 -> a1 -> a2 -> [a2] -> Bool
+isValidPart1 minl maxl l word = minl <= a && a <= maxl where a = sum [1 | c <- word, c == l]
+
+isValidPart2 :: Eq a => Int -> Int -> a -> [a] -> Bool
+isValidPart2 pos1 pos2 l word = (word !! (pos1 - 1) == l) /= (word !! (pos2 - 1) == l)
+
+splitBy :: (Foldable t, Eq a) => a -> t a -> [[a]]
+splitBy delimiter = foldr f [[]] 
+            where f c l@(x:xs) | c == delimiter = []:l
+                               | otherwise = (c:x):xs
+
+parseNums :: Foldable t => t Char -> [Int]
+parseNums str = map read (splitBy '-' str ):: [Int]
+
+
+processLine :: [Char] -> (Int -> Int -> Char -> String -> t) -> t
+processLine line f = f minl maxl letter word 
+        where
+            nums:letters:rest = words line
+            minl:maxl:_ = parseNums nums
+            letter = head letters
+            word = head rest
+
+
+processAll :: String -> (Int -> Int -> Char -> String -> Bool) -> Int
+processAll d f = sum [1 | x <- lines d, processLine x f]
+
+
+main = do
+    content <- getContents
+    print $ processAll content isValidPart1
+    print $ processAll content isValidPart2
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aoc2020-haskell
+
 Advent of Code in Haskell
+
+The code is mostly for my own educational purposes, so it isn't pretty
+
+So far I am not doing anything fancy, so compilation is done by `ghc main.hs`, running by `cat input.txt | ./main`. We'll see if this process will need improving
