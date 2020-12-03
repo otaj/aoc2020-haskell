5aa1e4eb63b4155db48490d1019b9a867b09bcba
Otakar Jašek <otakar.jasek@avast.com>
2020-12-03 17:14:31 +0100

    day 3

---
+++ 03/main.hs
@@ -0,0 +1,11 @@
+process right down startx starty content 
+    | starty >= length content = 0
+    | otherwise = process right down (startx + right) (starty + down) content + a
+        where v = content !! starty !! mod startx (length (content !! starty))
+              a = if v == '#' then 1 else 0
+
+
+main = do
+    content <- getContents
+    print $ process 3 1 0 0 (lines content)
+    print $ process 1 1 0 0 (lines content) * process 3 1 0 0 (lines content) * process 5 1 0 0 (lines content) * process 7 1 0 0 (lines content) * process 1 2 0 0 (lines content)
